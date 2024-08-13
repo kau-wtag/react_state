9358a9558557c96b4eaac4ecfd4f42d6a6d26129
udk-wtag <kaium.uddin@welldev.io>
2024-08-13 15:47:15 +0600

    implement reducer

--- src/ReducerExample/TaskApp.jsx
+++ src/ReducerExample/TaskApp.jsx
@@ -1,35 +1,39 @@
-import { useState } from "react";
+import { useReducer } from "react";
 import AddTask from "./AddTask";
 import TaskList from "./TaskList";
+import taskReducer from "./reducers/taskReducer";
+
+let nextId = 3;
+const initialTasks = [
+  { id: 0, text: "Learn C++", done: false },
+  { id: 1, text: "Go to Gym", done: false },
+  { id: 2, text: "Finish home work", done: false },
+];
+
 
 export default function TaskApp() {
-  const [tasks, setTasks] = useState(initialTasks);
+  const [tasks, dispatch] = useReducer(taskReducer, initialTasks);
 
   function handleAddTask(text) {
-    setTasks([
-      ...tasks,
-      {
-        id: nextId++,
-        text: text,
-        done: false,
-      },
-    ]);
+    dispatch({
+      type: "added",
+      id: nextId++,
+      text: text,
+    });
   }
 
   function handleChangeTask(task) {
-    setTasks(
-      tasks.map((t) => {
-        if (t.id === task.id) {
-          return task;
-        } else {
-          return t;
-        }
-      })
-    );
+    dispatch({
+      type: "changed",
+      task: task,
+    });
   }
 
   function handleDeleteTask(taskId) {
-    setTasks(tasks.filter((t) => t.id !== taskId));
+    dispatch({
+      type: "deleted",
+      id: taskId,
+    });
   }
 
   return (
@@ -45,9 +49,3 @@ export default function TaskApp() {
   );
 }
 
-let nextId = 3;
-const initialTasks = [
-  { id: 0, text: "Visit Kafka Museum", done: true },
-  { id: 1, text: "Watch a puppet show", done: false },
-  { id: 2, text: "Lennon Wall pic", done: false },
-];
