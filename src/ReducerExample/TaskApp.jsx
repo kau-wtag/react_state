f4ed3e9ab441e32f7d7c8b54a87b223b8ba55362
udk-wtag <kaium.uddin@welldev.io>
2024-08-13 16:11:03 +0600

    asda

--- src/ReducerExample/TaskApp.jsx
+++ src/ReducerExample/TaskApp.jsx
@@ -3,13 +3,11 @@ import AddTask from "./AddTask";
 import TaskList from "./TaskList";
 import taskReducer from "./taskReducer";
 
-let nextId = 3;
 const initialTasks = [
   { id: 0, text: "Learn C++", done: false },
   { id: 1, text: "Go to Gym", done: false },
   { id: 2, text: "Finish home work", done: false },
 ];
-
 
 export default function TaskApp() {
   const [tasks, dispatch] = useReducer(taskReducer, initialTasks);
@@ -17,7 +15,7 @@ export default function TaskApp() {
   function handleAddTask(text) {
     dispatch({
       type: "added",
-      id: nextId++,
+      id: Math.random(),
       text: text,
     });
   }
@@ -38,7 +36,7 @@ export default function TaskApp() {
 
   return (
     <>
-      <h1>Prague itinerary</h1>
+      <h1>use reducer - task app</h1>
       <AddTask onAddTask={handleAddTask} />
       <TaskList
         tasks={tasks}
