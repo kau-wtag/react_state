9839cc7c5e7efdfefa5d66d31256f1833df5f999
udk-wtag <kaium.uddin@welldev.io>
2024-08-14 17:58:27 +0600

    cache for data

--- src/CustomHooksExample/useFetch.js
+++ src/CustomHooksExample/useFetch.js
@@ -1,18 +1,29 @@
 import { useEffect, useState } from "react";
 
+const cache = {};
+
 function useFetch(model) {
   const [data, setData] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
   useEffect(() => {
+    if (cache[model]) {
+      setData(cache[model]);
+      setLoading(false);
+      return;
+    }
+
     const fetchData = async () => {
       try {
-        const response = await fetch("https://jsonplaceholder.typicode.com/" + model);
+        const response = await fetch(
+          "https://jsonplaceholder.typicode.com/" + model
+        );
         if (!response.ok) {
           throw new Error("Network response was not ok");
         }
         const result = await response.json();
+        cache[model] = result;
         setData(result);
       } catch (error) {
         setError(error);
