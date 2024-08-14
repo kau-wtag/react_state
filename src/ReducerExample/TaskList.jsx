629b90fd75d527b98a97ea895b32d24483fb71ed
udk-wtag <kaium.uddin@welldev.io>
2024-08-14 12:48:01 +0600

    context refactor

--- src/ReducerExample/TaskList.jsx
+++ src/ReducerExample/TaskList.jsx
@@ -1,5 +1,6 @@
-import { useContext, useState } from "react";
-import { TasksContext, TasksDispatchContext } from "./TasksContext";
+import { useContext } from "react";
+import { TasksContext } from "./TasksContext";
+import Task from "./Task";
 
 export default function TaskList() {
   const tasks = useContext(TasksContext);
@@ -15,63 +16,3 @@ export default function TaskList() {
   );
 }
 
-function Task({ task }) {
-  const [isEditing, setIsEditing] = useState(false);
-  const dispatch = useContext(TasksDispatchContext);
-  let taskContent;
-
-  if (isEditing) {
-    taskContent = (
-      <>
-        <input
-          value={task.text}
-          onChange={(e) => {
-            dispatch({
-              type: "changed",
-              task: {
-                ...task,
-                text: e.target.value,
-              },
-            });
-          }}
-        />
-        <button onClick={() => setIsEditing(false)}>Save</button>
-      </>
-    );
-  } else {
-    taskContent = (
-      <>
-        {task.text}
-        <button onClick={() => setIsEditing(true)}>Edit</button>
-      </>
-    );
-  }
-  return (
-    <label>
-      <input
-        type="checkbox"
-        checked={task.done}
-        onChange={(e) => {
-          dispatch({
-            type: "changed",
-            task: {
-              ...task,
-              done: e.target.checked,
-            },
-          });
-        }}
-      />
-      {taskContent}
-      <button
-        onClick={() => {
-          dispatch({
-            type: "deleted",
-            id: task.id,
-          });
-        }}
-      >
-        Delete
-      </button>
-    </label>
-  );
-}
--- src/ReducerExample/TasksContext.js
+++ src/ReducerExample/TasksContext.js
@@ -1,4 +1,4 @@
 import { createContext } from "react";
 
 export const TasksContext = createContext(null);
-export const TasksDispatchContext = createContext(null);
+export const TasksDispatchContext = createContext(null); 
