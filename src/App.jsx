d4f11fac33a24ed564a6693be952d0520f56d682
udk-wtag <kaium.uddin@welldev.io>
2024-08-15 15:15:51 +0600

    normal

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,5 @@
-import Image from "./Image";
-import Profile from "./Profile";
-
 function App() {
-  return (
-    <>
-      <Profile name={"kaium"} age={26} />
-      <Image
-        src={"https://images.unsplash.com/photo-1446776811953-b23d57bd21aa"}
-        alt={""}
-      />
-    </>
-  );
+  return <></>;
 }
 
 export default App;
